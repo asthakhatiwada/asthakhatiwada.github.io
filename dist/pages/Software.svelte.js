0bc629bfd2f012048deacded130a9b9441d603a2
asthakhatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2021-02-24 15:45:12 +0000

    Deploying to master from @ asthakhatiwada/profile-site@ce81cd5455ba6fd3977b521aeffc3d142161254c 🚀

--- dist/pages/Software.svelte.js
+++ dist/pages/Software.svelte.js
@@ -20,7 +20,7 @@ function create_fragment(ctx) {
 			div3.innerHTML = `<div class="block md:flex items-center"><div class="pr-4"><img class="m-auto" style="width: 150px" src="/public/img/gpatree.png"/></div> 
     <div class="flex-1"><h2 class="mt-0">GPATree</h2> 
       <p>R package to implement the GPA-Tree method.</p> 
-      <p>Shiny app to interactively implement association mapping and investigate
+      <p>R Shiny app to interactively implement association mapping and investigate
         functional annotation tree.</p> 
       <p>Download from
         <a class="external-link" href="https://github.com/asthakhatiwada/GPATree">Github</a>.</p></div></div>`;
