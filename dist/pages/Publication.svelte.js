06447de00dda2adee4d3ef18d7dfd6445808cee0
Aastha Khatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2021-09-24 13:19:19 -0600

    Update Publication.svelte.js

--- dist/pages/Publication.svelte.js
+++ dist/pages/Publication.svelte.js
@@ -141,18 +141,20 @@ function create_fragment(ctx) {
 			t41 = space();
 			div15 = element("div");
 
-			div15.innerHTML = `<div class="publication-item"><p>Song NJ, Allen C, Vilgelm. AE, 
+			div15.innerHTML = `<div class="publication-item">
+			
+			<div class="publication-item"><p><strong>Khatiwada A</strong>, Wolf BJ, Yilmaz AS, Ramos P, 
+			Pietrzak M, Lawson A, Hunt KJ, Kim HJ, Chung D. GPA-Tree: Statistical Approach for 
+			Functional-Annotation-Tree-Guided Prioritization of GWAS results. Submitted to
+			<span class="italic">Bioinformatics.</span></p></div> 
+  
+			<p>Song NJ, Allen C, Vilgelm. AE, 
 			Riesenberg BP, Weller KP, Reynolds K, Chakravarthy KB, Kumar A, <strong>Khatiwada A</strong>, Sun Z,
 			Ma A, Chang Y, Yusuf M, Li A, Zeng C, Evans JP, Bucci D, Gunasena M, Xu M, Liyanage NPM, 
 			Bolyard C, Velegraki M, Liu SL, Ma Q, Devenport M, Liu Y, Zheng P, Malvestutto CD, Chung D, Li Z. Immunological insights
       into the therapeutic roles of soluble CD24 against severe COVID-19.
       Submitted to
       <span class="italic">The Lanchet.</span></p></div> 
-  <div class="publication-item"><p><strong>Khatiwada A</strong>, Wolf BJ, Yilmaz AS, Ramos P, Pietrzak M,
-      Lawson A, Hunt KJ, Kim HJ, Chung D. GPA-Tree: Statistical Approach for
-      Functional-Annotation-Tree-Guided Prioritization of GWAS results.
-      Submitted to
-      <span class="italic">Bioinformatics.</span></p></div> 
   <div class="publication-item"><p>Ayoub I, Wolf BJ, Geng L, Song H,
       <strong>Khatiwada A</strong>, Tsao B, Oats J, Rovin B. Prediction Models
       of Treatment Response in Lupus Nephritis. Submitted to
