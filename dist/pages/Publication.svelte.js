c44b066aa833f1a320c3be4fbe9c442632cb969e
asthakhatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2021-09-28 20:20:51 +0000

    Deploying to master from @ asthakhatiwada/profile-site@4812db164fdbbf6151e12aac0f337fa9dd9b5183 🚀

--- dist/pages/Publication.svelte.js
+++ dist/pages/Publication.svelte.js
@@ -141,18 +141,19 @@ function create_fragment(ctx) {
 			t41 = space();
 			div15 = element("div");
 
-			div15.innerHTML = `<div class="publication-item"><p>The Pelotonia Institute for Immuno-Oncology, The Ohio State University
-      CD24Fc Covid19 Team (…,
-      <strong>Khatiwada A</strong>, …, Chung D, Li Z). Immunological insights
-      into the therapeutic roles of soluble CD24 against severe COVID-19.
-      Submitted to
-      <span class="italic">The New England Journal of Medicine.</span></p></div> 
-  <div class="publication-item"><p><strong>Khatiwada A</strong>, Wolf BJ, Yilmaz AS, Ramos P, Pietrzak M,
+			div15.innerHTML = `<div class="publication-item"><p><strong>Khatiwada A</strong>, Wolf BJ, Yilmaz AS, Ramos P, Pietrzak M,
       Lawson A, Hunt KJ, Kim HJ, Chung D. GPA-Tree: Statistical Approach for
       Functional-Annotation-Tree-Guided Prioritization of GWAS results.
       Submitted to
       <span class="italic">Bioinformatics.</span></p></div> 
-  <div class="publication-item"><p>Ayoub I, Wolf BJ, Geng L, Song H,
+  <div class="publication-item"><p>Song NJ, Allen C, Vilgelm AE, Riesenberg BP, Weller KP, Reynolds K, 
+      Chakravarthy KB, Kumar A, <strong>Khatiwada A</strong>, Sun Z, Ma A, Chang Y, Yusuf M,Li A, 
+      Zeng C, Evans JP, Bucci D, Gunasena M, Xu M, Liyanage NPM, Bolyard C, 
+      Velegraki M, Liu SL, Ma Q, Devenport M, Liu Y, Zheng P, Malvestutto CD#, 
+      Chung D#, Li Z#. Immunological insights into the therapeutic roles of 
+      soluble CD24 against severe COVID-19. Submitted to
+      <span class="italic">Cell Biology.</span></p></div> 
+ <div class="publication-item"><p>Ayoub I, Wolf BJ, Geng L, Song H,
       <strong>Khatiwada A</strong>, Tsao B, Oats J, Rovin B. Prediction Models
       of Treatment Response in Lupus Nephritis. Submitted to
       <span class="italic">Kidney International.</span></p></div> 
