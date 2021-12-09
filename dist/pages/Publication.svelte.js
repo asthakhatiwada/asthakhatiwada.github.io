c04daf1b83d5121ba00ddf120926a5c3bd1fc0d6
asthakhatiwada <69431135+asthakhatiwada@users.noreply.github.com>
2021-12-09 22:08:45 +0000

    Deploying to master from @ asthakhatiwada/profile-site@ca6d9418623a8451628aba5bd83ebd8c93bfc6ac 🚀

--- dist/pages/Publication.svelte.js
+++ dist/pages/Publication.svelte.js
@@ -25,36 +25,38 @@ import star from "../../public/heroicons/solid/star.svg.proxy.js";
 function create_fragment(ctx) {
 	let h10;
 	let t1;
-	let div11;
+	let div14;
 	let div2;
-	let t9;
-	let div5;
-	let div3;
 	let t10;
-	let div4;
-	let p1;
-	let t17;
-	let p2;
-	let inlinesvg0;
+	let div5;
 	let t18;
-	let t19;
 	let div8;
+	let div6;
+	let t19;
+	let div7;
+	let p2;
+	let t26;
+	let p3;
+	let inlinesvg0;
+	let t27;
 	let t28;
-	let div10;
-	let t36;
-	let h11;
-	let t38;
+	let div11;
+	let t37;
 	let div13;
-	let div12;
-	let p5;
 	let t45;
-	let p6;
-	let inlinesvg1;
-	let t46;
+	let h11;
 	let t47;
+	let div16;
+	let div15;
+	let p6;
+	let t54;
+	let p7;
+	let inlinesvg1;
+	let t55;
+	let t56;
 	let h12;
-	let t49;
-	let div19;
+	let t58;
+	let div21;
 	let current;
 
 	inlinesvg0 = new InlineSVG({
@@ -76,25 +78,34 @@ function create_fragment(ctx) {
 			h10 = element("h1");
 			h10.textContent = "Peer-reviewed Publications";
 			t1 = space();
-			div11 = element("div");
+			div14 = element("div");
 			div2 = element("div");
 
 			div2.innerHTML = `<div class="image-wrapper"><img class="publication-image" src="/public/img/bioinformatics.gif"/></div> 
+    <div><p>Ayoub I, Wolf BJ, Geng L, Song H, <strong>Khatiwada A</strong>, Tsao BP, Oates JC, Rovin BH.
+        <a class="external-link" target="_blank" href="https://doi.org/10.1016/j.kint.2021.11.014">Prediction Models of Treatment Response in Lupus Nephritis</a>.
+        <span class="italic">Kidney Int.</span>
+        2021 Dec 3:S0085-2538(21)01079-6. doi: 10.1016/j.kint.2021.11.014. Epub ahead of print. PMID: 34871620.</p></div>`;
+
+			t10 = space();
+			div5 = element("div");
+
+			div5.innerHTML = `<div class="image-wrapper"><img class="publication-image" src="/public/img/bioinformatics.gif"/></div> 
     <div><p><strong>Khatiwada A</strong>, Wolf BJ, Yilmaz AS, Ramos PS, Pietrzak M, Lawson A, Hunt KJ, Kim HJ, Chung D.
         <a class="external-link" target="_blank" href="https://academic.oup.com/bioinformatics/advance-article-abstract/doi/10.1093/bioinformatics/btab802/6443109">GPA-Tree: 
           Statistical Approach for Functional-Annotation-Tree-Guided Prioritization of GWAS Results</a>.
         <span class="italic">Bioinformatics</span>
         2021 Nov 26:btab802. doi: 10.1093/bioinformatics/btab802. Epub ahead of print. PMID: 34849578.</p></div>`;
 
-			t9 = space();
-			div5 = element("div");
-			div3 = element("div");
-			div3.innerHTML = `<img class="publication-image" src="/public/img/pr.png"/>`;
-			t10 = space();
-			div4 = element("div");
-			p1 = element("p");
+			t18 = space();
+			div8 = element("div");
+			div6 = element("div");
+			div6.innerHTML = `<img class="publication-image" src="/public/img/pr.png"/>`;
+			t19 = space();
+			div7 = element("div");
+			p2 = element("p");
 
-			p1.innerHTML = `<strong>Khatiwada A</strong>, Wolf BJ, Mulligan JK, Shary JR, Hewison M,
+			p2.innerHTML = `<strong>Khatiwada A</strong>, Wolf BJ, Mulligan JK, Shary JR, Hewison M,
         Baatz JE, Newton DA, Hawrylowicz C, Hollis BW, Wagner CL.
         <a class="external-link" target="_blank" href="https://pubmed.ncbi.nlm.nih.gov/32311700/">Effects of vitamin D
           supplementation on circulating concentrations of growth factors and
@@ -102,14 +113,14 @@ function create_fragment(ctx) {
         <span class="italic">Pediatr Res.</span>
         2020 Apr; 20:1-9. PMID: 32311700.`;
 
-			t17 = space();
-			p2 = element("p");
+			t26 = space();
+			p3 = element("p");
 			create_component(inlinesvg0.$$.fragment);
-			t18 = text("\n        Highlighted in the Editor’s Focus for the February 2021 print issue of\n        Pediatric Research");
-			t19 = space();
-			div8 = element("div");
+			t27 = text("\n        Highlighted in the Editor’s Focus for the February 2021 print issue of\n        Pediatric Research");
+			t28 = space();
+			div11 = element("div");
 
-			div8.innerHTML = `<div class="image-wrapper"><img class="publication-image" src="/public/img/csam.gif"/></div> 
+			div11.innerHTML = `<div class="image-wrapper"><img class="publication-image" src="/public/img/csam.gif"/></div> 
     <div><p>Nam JH*,
         <strong>Khatiwada A*</strong>, Matthews LJ, Schulte BA, Dubno JR, Chung
         D.
@@ -119,10 +130,10 @@ function create_fragment(ctx) {
         2020 Mar;27(2):225-239. PMID: 32566544; PMCID: PMC7304553 (*joint first
         author).</p></div>`;
 
-			t28 = space();
-			div10 = element("div");
+			t37 = space();
+			div13 = element("div");
 
-			div10.innerHTML = `<div class="image-wrapper"><img class="publication-image" src="/public/img/po.gif"/></div> 
+			div13.innerHTML = `<div class="image-wrapper"><img class="publication-image" src="/public/img/po.gif"/></div> 
     <p><strong>Khatiwada A</strong>, Shoaibi A, Neelon B, Emond JA,
       Benjamin-Neelon SE.
       <a class="external-link" target="_blank" href="https://pubmed.ncbi.nlm.nih.gov/30019385/">Household chaos during
@@ -130,41 +141,37 @@ function create_fragment(ctx) {
       <span class="italic">Pediatr Obes.</span>
       2018 Oct;13(10):607-613. PMID: 30019385; PMCID: PMC6300983.</p>`;
 
-			t36 = space();
+			t45 = space();
 			h11 = element("h1");
 			h11.textContent = "Master's Thesis";
-			t38 = space();
-			div13 = element("div");
-			div12 = element("div");
-			p5 = element("p");
+			t47 = space();
+			div16 = element("div");
+			div15 = element("div");
+			p6 = element("p");
 
-			p5.innerHTML = `<strong>Khatiwada A</strong>. 
+			p6.innerHTML = `<strong>Khatiwada A</strong>. 
       <a class="external-link" target="_blank" href="https://dc.etsu.edu/etd/3090/.">Multilevel Models for Longitudinal Data</a>.
       <span class="italic">Electronic Theses and Dissertations.</span>
       2016; Paper 3090.`;
 
-			t45 = space();
-			p6 = element("p");
+			t54 = space();
+			p7 = element("p");
 			create_component(inlinesvg1.$$.fragment);
-			t46 = text("\n        Downloaded over 2,100 times");
-			t47 = space();
+			t55 = text("\n        Downloaded over 2,100 times");
+			t56 = space();
 			h12 = element("h1");
 			h12.textContent = "Manuscript under Review";
-			t49 = space();
-			div19 = element("div");
+			t58 = space();
+			div21 = element("div");
 
-			div19.innerHTML = `<div class="publication-item"><p>Song NJ, Allen C, Vilgelm AE, Riesenberg BP, Weller KP, Reynolds K, 
+			div21.innerHTML = `<div class="publication-item"><p>Song NJ, Allen C, Vilgelm AE, Riesenberg BP, Weller KP, Reynolds K, 
       Chakravarthy KB, Kumar A, <strong>Khatiwada A</strong>, Sun Z, Ma A, Chang Y, Yusuf M,Li A, 
       Zeng C, Evans JP, Bucci D, Gunasena M, Xu M, Liyanage NPM, Bolyard C, 
       Velegraki M, Liu SL, Ma Q, Devenport M, Liu Y, Zheng P, Malvestutto CD, 
       Chung D, Li Z. Immunological insights into the therapeutic roles of 
       soluble CD24 against severe COVID-19. Submitted to
       <span class="italic">Journal of Hematology &amp; Oncology.</span></p></div> 
- <div class="publication-item"><p>Ayoub I, Wolf BJ, Geng L, Song H,
-      <strong>Khatiwada A</strong>, Tsao B, Oats J, Rovin B. Prediction Models
-      of Treatment Response in Lupus Nephritis. Submitted to
-      <span class="italic">Kidney International.</span></p></div> 
-  <div class="publication-item"><p>Richard MLL, Wirth JR,
+ <div class="publication-item"><p>Richard MLL, Wirth JR,
       <strong>Khatiwada A</strong>, Chung D, Eudaly J, Gilkeson GS, Cunningham
       MA. Conditional knockout of ERα in CD11c+ cells, impact on survival and inflammatory cytokines in murine lupus. Submitted to
       <span class="italic">The Journal of Immunology.</span></p></div> 
@@ -175,49 +182,52 @@ function create_fragment(ctx) {
 
 			attr(h10, "class", "text-xl font-bold mb-4");
 			attr(div2, "class", "publication-item md:flex");
-			attr(div3, "class", "image-wrapper");
-			attr(p2, "class", "special-text svelte-zfisdh");
 			attr(div5, "class", "publication-item md:flex");
+			attr(div6, "class", "image-wrapper");
+			attr(p3, "class", "special-text svelte-zfisdh");
 			attr(div8, "class", "publication-item md:flex");
-			attr(div10, "class", "publication-item md:flex");
+			attr(div11, "class", "publication-item md:flex");
+			attr(div13, "class", "publication-item md:flex");
 			attr(h11, "class", "text-xl font-bold mb-4");
-			attr(p6, "class", "special-text svelte-zfisdh");
-			attr(div12, "class", "publication-item");
+			attr(p7, "class", "special-text svelte-zfisdh");
+			attr(div15, "class", "publication-item");
 			attr(h12, "class", "text-xl font-bold mb-4");
 		},
 		m(target, anchor) {
 			insert(target, h10, anchor);
 			insert(target, t1, anchor);
-			insert(target, div11, anchor);
-			append(div11, div2);
-			append(div11, t9);
-			append(div11, div5);
-			append(div5, div3);
-			append(div5, t10);
-			append(div5, div4);
-			append(div4, p1);
-			append(div4, t17);
-			append(div4, p2);
-			mount_component(inlinesvg0, p2, null);
-			append(p2, t18);
-			append(div11, t19);
-			append(div11, div8);
-			append(div11, t28);
-			append(div11, div10);
-			insert(target, t36, anchor);
+			insert(target, div14, anchor);
+			append(div14, div2);
+			append(div14, t10);
+			append(div14, div5);
+			append(div14, t18);
+			append(div14, div8);
+			append(div8, div6);
+			append(div8, t19);
+			append(div8, div7);
+			append(div7, p2);
+			append(div7, t26);
+			append(div7, p3);
+			mount_component(inlinesvg0, p3, null);
+			append(p3, t27);
+			append(div14, t28);
+			append(div14, div11);
+			append(div14, t37);
+			append(div14, div13);
+			insert(target, t45, anchor);
 			insert(target, h11, anchor);
-			insert(target, t38, anchor);
-			insert(target, div13, anchor);
-			append(div13, div12);
-			append(div12, p5);
-			append(div12, t45);
-			append(div12, p6);
-			mount_component(inlinesvg1, p6, null);
-			append(p6, t46);
 			insert(target, t47, anchor);
+			insert(target, div16, anchor);
+			append(div16, div15);
+			append(div15, p6);
+			append(div15, t54);
+			append(div15, p7);
+			mount_component(inlinesvg1, p7, null);
+			append(p7, t55);
+			insert(target, t56, anchor);
 			insert(target, h12, anchor);
-			insert(target, t49, anchor);
-			insert(target, div19, anchor);
+			insert(target, t58, anchor);
+			insert(target, div21, anchor);
 			current = true;
 		},
 		p: noop,
@@ -235,17 +245,17 @@ function create_fragment(ctx) {
 		d(detaching) {
 			if (detaching) detach(h10);
 			if (detaching) detach(t1);
-			if (detaching) detach(div11);
+			if (detaching) detach(div14);
 			destroy_component(inlinesvg0);
-			if (detaching) detach(t36);
+			if (detaching) detach(t45);
 			if (detaching) detach(h11);
-			if (detaching) detach(t38);
-			if (detaching) detach(div13);
-			destroy_component(inlinesvg1);
 			if (detaching) detach(t47);
+			if (detaching) detach(div16);
+			destroy_component(inlinesvg1);
+			if (detaching) detach(t56);
 			if (detaching) detach(h12);
-			if (detaching) detach(t49);
-			if (detaching) detach(div19);
+			if (detaching) detach(t58);
+			if (detaching) detach(div21);
 		}
 	};
 }
